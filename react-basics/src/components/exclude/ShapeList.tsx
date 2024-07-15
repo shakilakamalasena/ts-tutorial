9f78a5a0950029c09a7e3b89f724d23063bee23e
Shakila Kamalasena <104765058+shakilakamalasena@users.noreply.github.com>
2024-07-15 14:35:40 +0530

    TypeScript Combined Types and Exclude

---
+++ react-basics/src/components/exclude/ShapeList.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Shape from "./Shape";
+
+const ShapeList = () => {
+    return (
+        <div>
+            <Shape color="light - yellow" />
+        </div>
+    );
+};
+
+export default ShapeList;
